bc640e020f4acb9c317631a0728829576e3e17e9
m-hichikalov <m.chichikalov@outlook.com>
2017-11-02 22:15:43 -0500

    add test -> test_get_free_space_in_buffer.

--- test/test_circular_buffer_char.c
+++ test/test_circular_buffer_char.c
@@ -2,6 +2,7 @@
  *    INCLUDED FILES
  ******************************************************************************/
 //-- unity: unit test framework
+#include <stdlib.h>
 #include "unity.h"
 //-- module being tested
 #define TYPE_OF_ELEMENT_OF_BUFFER     char
@@ -24,8 +25,10 @@ circular_buffer_t rx_buffer;
 /*******************************************************************************
  *    PRIVATE FUNCTIONS
  ******************************************************************************/
-extern TYPE_OF_ELEMENT_OF_BUFFER* c_b_get_p_actual(circular_buffer_t *pThis);
-extern TYPE_OF_ELEMENT_OF_BUFFER* c_b_get_p_used(circular_buffer_t *pThis);
+extern TYPE_OF_ELEMENT_OF_BUFFER* c_b_get_pHead(circular_buffer_t *pThis);
+extern TYPE_OF_ELEMENT_OF_BUFFER* c_b_get_pTail(circular_buffer_t *pThis);
+extern void c_b_move_pHead(circular_buffer_t * pThis);
+extern void c_b_move_pTail(circular_buffer_t* pThis);
  /*******************************************************************************
  *    SETUP, TEARDOWN
  ******************************************************************************/
@@ -45,27 +48,27 @@ void test_init_circular_buffer(void){
 	// -- Check initialization function. Kind of ugly because the last always returns 1.
     TEST_ASSERT_EQUAL_INT(1 , c_b_init(&rx_buffer));
     // -- Test that pointer to head and tail is equal.
-    TEST_ASSERT_EQUAL_PTR(c_b_get_p_actual(&rx_buffer), c_b_get_p_used(&rx_buffer));
+    TEST_ASSERT_EQUAL_PTR(c_b_get_pHead(&rx_buffer), c_b_get_pTail(&rx_buffer));
     // -- Test that pointer to head points to the first element.
-    TEST_ASSERT_EQUAL_PTR(&rx_buffer, c_b_get_p_actual(&rx_buffer));
+    TEST_ASSERT_EQUAL_PTR(&rx_buffer, c_b_get_pHead(&rx_buffer));
 }
 
-void test_full_buffer(void){
-	const char *str_to_long = "1234567897878";
-	TEST_ASSERT_EQUAL_INT(
-			-1,
-			c_b_put_string(&rx_buffer, str_to_long)
-			);
-}
-
-void test_full_buffer_ok(void){
-	const char *str_ok = "1234";
-	TEST_ASSERT_EQUAL_INT(
-			0,
-			c_b_put_string(&rx_buffer, str_ok)
-			);
-}
-
+//void test_full_buffer(void){
+//	const char *str_to_long = "1234567897878";
+//	TEST_ASSERT_EQUAL_INT(
+//			-1,
+//			c_b_put_string(&rx_buffer, str_to_long)
+//			);
+//}
+//
+//void test_full_buffer_ok(void){
+//	const char *str_ok = "1234";
+//	TEST_ASSERT_EQUAL_INT(
+//			0,
+//			c_b_put_string(&rx_buffer, str_ok)
+//			);
+//}
+//
 void test_put_one_element_into_buffer(void){
 	char a = 'a';
 	char b = 'b';
@@ -82,12 +85,35 @@ void test_put_one_element_into_buffer(void){
 
 }
 
+//void test_make_buffer_full(void){
+//	char a = 'a';
+//	int i;
+//	for (i = 0; i < 8; i++){
+//		c_b_put(&rx_buffer, &a);
+//	}
+//	TEST_ASSERT_EQUAL_INT(
+//			-1,
+//			c_b_put(&rx_buffer, &a)
+//			);
+//}
 
-
-
-
-
-
+void test_get_free_space_in_buffer(void){
+	int i;
+	// -- Empty buffer is able to take (LEN_BUFFER-1) elements.
+	TEST_ASSERT_EQUAL_INT((LEN_BUFFER-1), c_b_get_free_space(&rx_buffer));
+	for (i=0; i<5; i++){
+		c_b_move_pHead(&rx_buffer);
+	}
+	TEST_ASSERT_EQUAL_INT(2, c_b_get_free_space(&rx_buffer));
+	for (i=0; i<7; i++){
+		c_b_move_pTail(&rx_buffer);
+	}
+	TEST_ASSERT_EQUAL_INT(1, c_b_get_free_space(&rx_buffer));
+	c_b_move_pTail(&rx_buffer);
+	TEST_ASSERT_EQUAL_INT(2, c_b_get_free_space(&rx_buffer));
+	// -- made one circle around, ptr of Tail should points to first element.
+	TEST_ASSERT_EQUAL_PTR(&rx_buffer, c_b_get_pTail(&rx_buffer));
+}
 
 
 
